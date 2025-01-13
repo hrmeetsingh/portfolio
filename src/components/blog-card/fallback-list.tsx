3d3a4b72369f379765fbecbae9eb18df4c555eb6
Harmeet Singh <er.harmeetsingh@hotmail.com>
2025-01-12 22:44:26 -0700

    Update blogs

--- src/components/blog-card/fallback-list.tsx
+++ src/components/blog-card/fallback-list.tsx
@@ -1,34 +1,42 @@
 export const wpBlogs = [
-    {
-        title: 'SpecFlow and Selenium WebDriver - An alternative approach to hybrid test automation framework',
-        thumbnail: 'https://hrmeetsingh.wordpress.com/wp-content/uploads/2015/09/frameworkarchitecture_watermarked.png',
-        link: 'https://hrmeetsingh.wordpress.com/2015/09/07/specflow-and-selenium-webdriver-an-alternative-approach-to-hybrid-automation-framework/',
-        publishedAt: new Date('09/07/2015'),
-        description: 'Test Automation Frameworks – By definition, are there between the user and the Application Under Test, provide ground rules, reusable components, exception handling, fallback mechanisms and reporting to the users.',
-        categories: ['CSharp','specflow','automation','selenium', 'BDD', 'WebDriver', 'Framework']
-      },
-      {
-        title: 'Linux Kernel Module - 101',
-        thumbnail: 'https://hrmeetsingh.wordpress.com/wp-content/uploads/2016/07/screen-shot-2016-07-19-at-4-03-52-pm.png',
-        link: 'https://hrmeetsingh.wordpress.com/2016/07/19/linux-kernel-modules-101/',
-        publishedAt: new Date('07/19/2016'),
-        description: 'Linux kernel is an operating system kernel which acts as a bridge between the user and the computer hardware. It is the first program to be loaded into the memory when the system boots and takes care of other programs like reading/writing from devices or peripherals attached to the computer once its up.',
-        categories: ['Linux','Kernel','Module','LoadableKernelModule']
-      },
-      {
-        title: 'Python-OpenCV for detecting colored object',
-        thumbnail: 'https://hrmeetsingh.wordpress.com/wp-content/uploads/2015/08/opencvdialogs.jpg',
-        link: 'https://hrmeetsingh.wordpress.com/2015/08/19/python-opencv-for-detecting-colored-object/',
-        publishedAt: new Date('08/15/2015'),
-        description: 'Machines too got sensory organs. The one that really fascinates me is “Vision”. OpenCV, or Open Computer Vision is such a program library that provides computers the ability to see through a camera and analyse the images.',
-        categories: ['ComputerVision','Python','OpenCV','ObjectTracking']
-      },
-      {
-        title: 'Monitoring WebDriver Actions – Using WebDriverEventListener and EventFiringWebDriver',
-        thumbnail: 'https://hrmeetsingh.wordpress.com/wp-content/uploads/2015/09/frameworkarchitecture_watermarked.png',
-        link: 'https://hrmeetsingh.wordpress.com/2015/09/17/monitoring-webdriver-actions-using-webdrivereventlistener-and-eventfiringwebdriver/',
-        publishedAt: new Date('09/17/2015'),
-        description: 'To throw an event, WebDriver gives a class named EventFiringWebDriver, and to catch that event, it provides an interface named WebDriverEventListener. Together, these two can be used to trigger an event, catch it and perform desired action.',
-        categories: ['Selenium','WebDriver','automation','selenium','Java','EventListener']
-      }
+  {
+    title: 'Model Context Protocol and the use of AI Agents in automation testing',
+    thumbnail: 'https://hrmeetsingh.wordpress.com/wp-content/uploads/2015/09/frameworkarchitecture_watermarked.png',
+    link: 'https://miro.medium.com/v2/resize:fit:4800/format:webp/1*uuiXF4xMkU48AuFfXFX5BA.png',
+    publishedAt: new Date('12/29/2024'),
+    description: 'Model Context Protocol is a standard protocol to connect an LLM to an execution environment. In this blog, we will look at how to use Model Context Protocol to connect Anthropic Claude to Playwright and how to use AI agents in automation testing.',
+    categories: ['AI Agents', 'Claude AI', 'Playwright', 'automation', 'LLM', 'Model Context Protocol', 'Framework']
+  },
+  {
+    title: 'SpecFlow and Selenium WebDriver - An alternative approach to hybrid test automation framework',
+    thumbnail: 'https://hrmeetsingh.wordpress.com/wp-content/uploads/2015/09/frameworkarchitecture_watermarked.png',
+    link: 'https://hrmeetsingh.wordpress.com/2015/09/07/specflow-and-selenium-webdriver-an-alternative-approach-to-hybrid-automation-framework/',
+    publishedAt: new Date('09/07/2015'),
+    description: 'Test Automation Frameworks – By definition, are there between the user and the Application Under Test, provide ground rules, reusable components, exception handling, fallback mechanisms and reporting to the users.',
+    categories: ['CSharp', 'specflow', 'automation', 'selenium', 'BDD', 'WebDriver', 'Framework']
+  },
+  {
+    title: 'Linux Kernel Module - 101',
+    thumbnail: 'https://hrmeetsingh.wordpress.com/wp-content/uploads/2016/07/screen-shot-2016-07-19-at-4-03-52-pm.png',
+    link: 'https://hrmeetsingh.wordpress.com/2016/07/19/linux-kernel-modules-101/',
+    publishedAt: new Date('07/19/2016'),
+    description: 'Linux kernel is an operating system kernel which acts as a bridge between the user and the computer hardware. It is the first program to be loaded into the memory when the system boots and takes care of other programs like reading/writing from devices or peripherals attached to the computer once its up.',
+    categories: ['Linux', 'Kernel', 'Module', 'LoadableKernelModule']
+  },
+  {
+    title: 'Python-OpenCV for detecting colored object',
+    thumbnail: 'https://hrmeetsingh.wordpress.com/wp-content/uploads/2015/08/opencvdialogs.jpg',
+    link: 'https://hrmeetsingh.wordpress.com/2015/08/19/python-opencv-for-detecting-colored-object/',
+    publishedAt: new Date('08/15/2015'),
+    description: 'Machines too got sensory organs. The one that really fascinates me is “Vision”. OpenCV, or Open Computer Vision is such a program library that provides computers the ability to see through a camera and analyse the images.',
+    categories: ['ComputerVision', 'Python', 'OpenCV', 'ObjectTracking']
+  },
+  {
+    title: 'Monitoring WebDriver Actions – Using WebDriverEventListener and EventFiringWebDriver',
+    thumbnail: 'https://hrmeetsingh.wordpress.com/wp-content/uploads/2015/09/frameworkarchitecture_watermarked.png',
+    link: 'https://hrmeetsingh.wordpress.com/2015/09/17/monitoring-webdriver-actions-using-webdrivereventlistener-and-eventfiringwebdriver/',
+    publishedAt: new Date('09/17/2015'),
+    description: 'To throw an event, WebDriver gives a class named EventFiringWebDriver, and to catch that event, it provides an interface named WebDriverEventListener. Together, these two can be used to trigger an event, catch it and perform desired action.',
+    categories: ['Selenium', 'WebDriver', 'automation', 'selenium', 'Java', 'EventListener']
+  }
 ]
