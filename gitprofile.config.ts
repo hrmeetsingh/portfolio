21b015dad1cb658c348659fb831d94e1cd08de7f
Harmeet Singh <er.harmeetsingh@hotmail.com>
2025-01-12 23:00:45 -0700

    change theme

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -233,11 +233,11 @@ const CONFIG = {
     snippetVersion: 6,
   },
   themeConfig: {
-    defaultTheme: 'black',
+    defaultTheme: 'Wireframe',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
-    disableSwitch: false,
+    disableSwitch: true,
 
     // Should use the prefers-color-scheme media-query,
     // using user system preferences, instead of the hardcoded defaultTheme
